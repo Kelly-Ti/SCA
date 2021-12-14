bfb1e288e2c2220985d04eb59fef5e43a1494713
Tianying Xun <kelly@Tianyings-MacBook-Pro.local>
2021-12-14 15:15:43 +1100

    style draft

--- src/page-components/Category/components/CategoryCard/index.js
+++ src/page-components/Category/components/CategoryCard/index.js
@@ -2,12 +2,34 @@ import React from 'react';
 
 function CategoryCard({ item }) {
   return (
-    <div className="cat-card">
+    <div className="cat-card" style={{ width: '320px' }}>
       <div className="cat-picture">
         <img className="cat-img" src={item.images.squareLarge.url}></img>
       </div>
-      <h2 className="cat-title">{item.name}</h2>
-      <p className="cat-desc">{item.description}</p>
+      <h2
+        className="cat-title"
+        style={{
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          display: '-webkit-box',
+          '-webkit-line-clamp': '1',
+          '-webkit-box-orient': 'vertical',
+        }}
+      >
+        {item.name}{' '}
+      </h2>
+      <p
+        className="cat-desc"
+        style={{
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          display: '-webkit-box',
+          '-webkit-line-clamp': '3',
+          '-webkit-box-orient': 'vertical',
+        }}
+      >
+        {item.description}
+      </p>
     </div>
   );
 }
